02297432f7447ee9819c7ec09a05b9f888418484
Diana Kim <dianakim@MacBook-Air-Diana.local>
2026-03-20 23:21:25 +0900

    8.4

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 2,
-    spaceBetween: 10,
+    slidesPerView: "auto",
+    spaceBetween: 17,
     direction: 'horizontal',
-    grabCursor: true,
 
-    //  mousewheel: true,
-    //  watchOverflow: true,
-    // resistanceRatio: 0,
-    // watchOverflow: true,
-freeMode: true,
-freeModeSticky: true,
-    centeredSlidesBounds: true,
     slidesOffsetAfter: 10,
-    slidesPerSetBefore: 20,
-    slidesPerGroupAuto: true,
-    // slideTiClickedSlide: true,
-    // loop: true,
+    breakpoints: {
+        768: {
+            enabled: false,
+        }
+    },
 
-    // touchRatio: 1.2,
-    // longSwipesRatio: 0.2,
-    // speed: 500,
-    // resistanceRatio: 0,
-   
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 });
-//  const btn = document.querySelectorAll(".logo-button");
-//  buttons.forEach(btn => {
-//  btn.style.width = "240px";
-//  btn.style.height = "72px";
-//  });
-// buttons.forEach(btn => {
-//     btn.classList.add("logo-button");
-// });
+
 
 const showBtn = document.querySelector("#showBtn");
 const hideBtn = document.querySelector("#hideBtn");
@@ -52,8 +33,8 @@ hideBtn.addEventListener("click", () => {
     showBtn.classList.remove("hidden");
 });
 
-const showBtn2 = document.querySelector("#showBtn");
-const hideBtn2 = document.querySelector("#hideBtn");
+const showBtn2 = document.querySelector("#showBtn2");
+const hideBtn2 = document.querySelector("#hideBtn2");
 const buttons2 = document.querySelectorAll(".button3");
 
 showBtn2.addEventListener("click", () => {
@@ -67,3 +48,5 @@ hideBtn2.addEventListener("click", () => {
     hideBtn2.classList.add("hidden2");
     showBtn2.classList.remove("hidden2");
 });
+
+
